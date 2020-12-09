0841eaca2faa9923824d5a323a77138f500fb0a7
Alex <cigan@ua.fm>
2020-12-09 19:30:27 +0200

    practice with listeners as v-on:click / input / keyup and their modifications

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const app = Vue.createApp({
     data() {
         return {
-            userName: 'Alexander Tsyh',
-            userAge: '37',
-            imageLink: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
-        }
+            counter: 0,
+            name: ''
+        };
     },
     methods: {
-        userAgeAdd5() {
-            const toNumber = Number.parseInt(this.userAge);
-            const userAgeAdd5 = toNumber + 5;
-            return userAgeAdd5;
+        add(num) {
+            this.counter = this.counter + num;
         },
-        randomNumber() {
-            randomNumber = Math.random();
-            return Math.round(randomNumber);
+        reduce(num) {
+            this.counter = this.counter - num;
+        },
+        changeName(lastName) {
+            this.name = event.target.value + ' ' + lastName;
+        },
+        submitForm() {
+            alert('Submitted!')
+        },
+        confirmedText(event, lastName) {
+            this.name = event.target.value + ' ' + lastName;
         }
+
     }
 });
 
-app.mount('#assignment');
+app.mount('#events');
