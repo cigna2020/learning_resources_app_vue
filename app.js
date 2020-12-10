4a453f9add35fa4e4b06f085d29132731fd50c3d
Alex <cigan@ua.fm>
2020-12-10 19:19:18 +0200

    Add inline style

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            result: 0,
-            number: 37,
-            message: '',
-            messageNotYet: 'Not there yet',
-            messageTooMuch: 'Too much!'
+            boxA: false,
+            boxB: false,
+            boxC: false,
         }
     },
+
     methods: {
-        addNumber(num) {
-            this.result += num;
-        }
-    },
-    computed: {
-        checker() {
-            if (this.result === 0) {
-                return "It's time to start!";
+        selectedBox(box) {
+            if (box == 'A') {
+                this.boxA = true;
+            } else if (box == 'B') {
+                this.boxB = true;
+            } else if (box == 'C') {
+                this.boxC = true;
             }
-            if (this.result < this.number) {
-                return this.messageNotYet;
-            }
-            if (this.result > this.number) {
-                return this.messageTooMuch
-            }
-            if (this.result == this.number) {
-                return "Exactly!"
-            }
-        }
-    },
-    watch: {
-        result(value) {
-            const that = this;
-            function resetResult() {
-                that.result = 0
-            };
-            setTimeout(resetResult, 2000)
         }
     }
-
 });
 
-app.mount('#assignment');
+app.mount('#styling');
