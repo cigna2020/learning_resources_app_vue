25391b2a6da9c2e013b0394a09aa03c647c8f604
Alex <cigan@ua.fm>
2020-12-12 13:54:18 +0200

    The practice adding styles and classes using v-model

--- app.js
+++ app.js
@@ -3,43 +3,49 @@ const app = Vue.createApp({
         return {
             name: '',
             visible: true,
-            newBackgroundColor: '',
+            // newBackgroundColor: '',
+            inputStyle: '',
         }
     },
     methods: {
-        changeName() {
-            this.name = event.target.value;
-        },
+        // changeName() {
+        //     this.name = event.target.value;
+        // },
         toggleVisible() {
-            if (!this.visible) {
-                console.log('false');
-                this.visible = true;
-                return 'hidden';
-            } if (this.visible) {
-                console.log('true');
-                this.visible = false;
-                return 'visible';
-            }
+            this.visible = !this.visible;
+            // if (!this.visible) {
+            //     console.log('false');
+            //     this.visible = true;
+            //     return 'hidden';
+            // } if (this.visible) {
+            //     console.log('true');
+            //     this.visible = false;
+            //     return 'visible';
+            // }
         },
-        changeBGC() {
-            this.newBackgroundColor = event.target.value;
-            return this.newBackgroundColor;
-        }
+        // changeBGC() {
+        //     this.newBackgroundColor = event.target.value;
+        //     return this.newBackgroundColor;
+        // }
 
     },
     computed: {
-        userClass() {
-            if (this.name == 'user1') {
-                return 'user1'
-            } if (this.name == 'user2') {
-                return 'user2'
-            }
-        },
+        // userClass() {
+        //     if (this.name == 'user1') {
+        //         return 'user1'
+        //     } if (this.name == 'user2') {
+        //         return 'user2'
+        //     }
+        // },
         toggleClass() {
-            if (this.visible) {
-                return 'hidden';
-            } else {
-                return 'visible';
+            // if (this.visible) {
+            //     return 'hidden';
+            // } else {
+            //     return 'visible';
+            // }
+            return {
+                visible: this.visible,
+                hidden: !this.visible
             }
         },
 
