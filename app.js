6fcbeb355f3a4f1c237b1abecf13b7a22d77dc8f
Alex <cigan@ua.fm>
2020-12-12 16:08:56 +0200

    Example of using v-if, v-else and v-show

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            name: '',
-            visible: true,
-            // newBackgroundColor: '',
-            inputStyle: '',
-        }
+            goals: [],
+            enteredGoal: '',
+        };
     },
     methods: {
-        // changeName() {
-        //     this.name = event.target.value;
-        // },
-        toggleVisible() {
-            this.visible = !this.visible;
-            // if (!this.visible) {
-            //     console.log('false');
-            //     this.visible = true;
-            //     return 'hidden';
-            // } if (this.visible) {
-            //     console.log('true');
-            //     this.visible = false;
-            //     return 'visible';
-            // }
-        },
-        // changeBGC() {
-        //     this.newBackgroundColor = event.target.value;
-        //     return this.newBackgroundColor;
-        // }
-
-    },
-    computed: {
-        // userClass() {
-        //     if (this.name == 'user1') {
-        //         return 'user1'
-        //     } if (this.name == 'user2') {
-        //         return 'user2'
-        //     }
-        // },
-        toggleClass() {
-            // if (this.visible) {
-            //     return 'hidden';
-            // } else {
-            //     return 'visible';
-            // }
-            return {
-                visible: this.visible,
-                hidden: !this.visible
-            }
-        },
-
+        addGoal() {
+            this.goals.push(this.enteredGoal);
+        }
     }
 });
 
-app.mount('#assignment');
+app.mount('#user-goals');
