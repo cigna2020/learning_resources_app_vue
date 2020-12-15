b26d7f7f9b48aeb1771ef690fb37195faa761f4e
Alex <cigan@ua.fm>
2020-12-15 16:38:04 +0200

    Add computed property  "special attack" and methods "heal" a player

--- app.js
+++ app.js
@@ -6,17 +6,35 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            countAttack: 0,
         }
     },
     methods: {
         attackMonster() {
+            this.countAttack++;
             this.monsterHealth -= getRandomValue(5, 8);
             this.attackPlayer();
         },
         attackPlayer() {
             this.playerHealth -= getRandomValue(8, 15);
+        },
+        specialAttack() {
+            this.countAttack++;
+            this.monsterHealth -= getRandomValue(10, 25);
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.countAttack++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
+
     },
     computed: {
         monsterBarStyles() {
@@ -24,7 +42,11 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack() {
+            return this.countAttack % 3 !== 0;
         }
+
     }
 });
 
