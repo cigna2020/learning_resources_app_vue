00d36d50e7df51a62b3a6eb1751c84d70a1b6d28
Alex <cigan@ua.fm>
2020-12-16 17:58:16 +0200

    Add a template with html-code and a script with data-property

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manual",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Joenz",
+          phone: "0987 45678 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
