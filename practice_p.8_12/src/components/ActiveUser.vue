d0fc3c14ace0c429ff33e54b27ca104861bd0409
Alex <cigan@ua.fm>
2020-12-17 20:01:26 +0200

    Make practice exercises about components
    To render info from a data and add new data

---
+++ practice_p.8_12/src/components/ActiveUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div>
+      <h2><strong>User name:</strong> {{ name }}</h2>
+    </div>
+    <div>
+      <h3><strong>User age:</strong> {{ age }}</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["user-data"],
+  props: ["id", "name", "age"],
+};
+</script>
