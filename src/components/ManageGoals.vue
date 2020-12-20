8a34480b4598e0485937e6ca1047b3518b934863
Alex <cigan@ua.fm>
2020-12-20 17:22:47 +0200

    Make a modal window using the component and tag <dialog>

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,35 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" placeholder="some text" />
+  <input type="text" placeholder="Your text here..." ref="goal" />
+  <button @click="setGoal">Confirm!</button>
+  <error-alert v-if="inputIsValid">
+    <h2>The input shouldn't be empty!</h2>
+    <p>Please enter some text...</p>
+    <button @click="confirmError">Ok</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsValid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsValid = false;
+    },
+  },
+};
+</script>
