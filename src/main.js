3a9a10287fcd56c6ced0c93064aff90027291f60
Alex <cigan@ua.fm>
2020-12-21 15:11:57 +0200

    Add BaseButton component with the possibility to change a class and with style of a button

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import {createApp} from 'vue';
 import App from './App';
 
 import BaseCard from './components/UI/BaseCard'
+import BaseButtton from './components/UI/BaseButtton';
+
 
 const app = createApp(App);
 app.component('BaseCard', BaseCard)
+app.component('BaseButton', BaseButtton)
 
 app.mount('#app')
